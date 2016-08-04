5609eedb5c6299b95ec0acd2b7335ce21fd41777
jaeseok an <jaeseokan94@gmail.com>
2016-08-04 14:07:59 +0900

    optimised algorithms, removed creating a class object everytime in a for loop, and passing self loop using less memory

--- Algorithms-Design-Analysis/week3-GraphMinimumCut/KargerMinCut.java
+++ Algorithms-Design-Analysis/week3-GraphMinimumCut/KargerMinCut.java
@@ -1,92 +1,28 @@
 package algorithms;
 
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
+import java.io.*;
 
 public class KargerMinCut {
 	
-	public static Integer[] convertStringToInt(String array[]){
-		Integer[] intArr = new Integer[array.length];
-		for(int i = 0 ; i < array.length; i++) 
-			intArr[i] = Integer.parseInt(array[i]);
-		
-		return intArr;
-			
-	}
-	
-	public static Integer[] extractEdges(Integer arr[]){
-	
-		Integer edgesArr[] = new Integer[arr.length-1];
-		for(int i = 1 ; i < arr.length ; i ++ ){ // ignore first value of array since it represents vertex not edges
-			edgesArr[i-1] = arr[i];
-		}
-		
-		return edgesArr;
-	}
-	
-	public HashMap<Integer,Integer[]> input() throws IOException{
-		String input = "src/algorithms/input/mincut.txt";
-		String inputTest = "src/algorithms/input/test.txt";
-		
-		FileInputStream fis = new FileInputStream(input);
-        DataInputStream in = new DataInputStream(fis);
-        BufferedReader br = new BufferedReader(new InputStreamReader(in));
-		
-		
-		
-		//int vertices[];
-		//int[] edges[];
-
-		ArrayList vertices = new ArrayList();
-		ArrayList<Integer[]> edges = new ArrayList<Integer[]>();
-		
-		HashMap<Integer,Integer[]> graph = new HashMap<Integer,Integer[]>();  
-		
-		String v;
-		int count = 1;
-		int N = 200;
-		
-		while(count<=N){
-			Integer arr[];
-
-           arr = convertStringToInt(br.readLine().split("	"));
-           edges.add(extractEdges(arr));
-           vertices.add(arr[0]);
-           
-           graph.put(arr[0],extractEdges(arr));
-           
-           count++;
-		}
-		
-		return graph;
-		
-	}
-			
 	public static void main(String[] args) throws IOException {
+    	double a1,a2;
+		a1 = System.nanoTime();
 		
-		
-		
+    	KargerMinCut k = new KargerMinCut();
+    	
 		int ans=100000;
-        for(int i = 0 ; i < 2000 ; i++){
-        	KargerMinCut k = new KargerMinCut();
+        for(int i = 0 ; i < 500 ; i++){
     		int tmp = k.karger(k.input());
         	if(tmp<ans){
         		ans=tmp;
         	}
         }
 		
-		
-    System.out.println("ans!!   "+ans);
+		a2 = System.nanoTime();
+
+    System.out.println("Minimum cut =  "+ans);
+    System.out.println("time taken " + (a2-a1));
         
 	}
 	
@@ -94,65 +30,42 @@ public class KargerMinCut {
 		
         Random r = new Random();
 
-		int counter = 0 ;
 		while(graph.size()>2){
 			
-			
-	        for (Map.Entry<Integer,Integer[]> entry : graph.entrySet()) {
+			// check if there are self loop
+			for (Map.Entry<Integer,Integer[]> entry : graph.entrySet()) {
 	        	  Integer key = entry.getKey();
 	        	  Integer[] value = entry.getValue();
 	        	  
 	        	  int d = 0 ;
 	        	  for(int val : value){
+	        		  
 	        		 if(key==val){
 	        			  graph.get(key)[d]=1000;
-	        			 // System.out.println("key = value");
-	        			 // System.out.println("key = "+key+"   d = "+d);
-	        		  }
-	        		  
+	        		  }	        		  
 	        		  d++;
 	        	  }
 	        	}
-	        
 			
-			List<Integer> keys      = new ArrayList<Integer>(graph.keySet());
+			
+			List<Integer> keys = new ArrayList<Integer>(graph.keySet());
 			int u = keys.get( r.nextInt(keys.size()) ); // picking up random u 
 			
-			Integer[] keysOfkeys     ;
+			Integer[] keysOfkeys;
 			keysOfkeys = graph.get(u);
 			
-			int idx = new Random().nextInt(keysOfkeys.length);
+			int idx = new Random().nextInt(keysOfkeys.length); // picking up another vertex from u
 
 			int v = (keysOfkeys[idx]);
 			// to avoid self-loop
-		//	System.out.println("enter");
-		//System.out.println(Arrays.toString(keysOfkeys));
 			 while(v==1000){
 				int idx2 = new Random().nextInt(keysOfkeys.length);
 				v = (keysOfkeys[idx2]);
-
 			 }
 			
-			
-	       // int u = r.nextInt(graph.size());      
-	       // int v =r.nextInt(graph.get(u).length);   // picking up random v 
-	     //  System.out.println("u = "+u +"   v = "+v);
-	      //  System.out.print("edge in while loop : ");
-	        	        
-	        for (Map.Entry<Integer,Integer[]> entry : graph.entrySet()) {
-	        	  Integer key = entry.getKey();
-	        	  Integer[] value = entry.getValue();
-	        	  //System.out.print("key : "+ key + " value : "+Arrays.toString(value)+"    ");
-	        	}
-	        
-			
-		
-		//	System.out.println("");
-	        Integer[] uG,vG = new Integer[400];
+	        Integer[] uG,vG = new Integer[200];
 	        uG = graph.get(u);
 	        vG = graph.get(v);
-	        
-	        
 	        
 			//System.out.println("uG = "+Arrays.toString(uG));
 			//System.out.println("vG = "+Arrays.toString(vG));
@@ -165,10 +78,11 @@ public class KargerMinCut {
 	        System.arraycopy(vG, 0, toMerge, graph.get(u).length, graph.get(v).length);
 	        
 	        
-	      //after merge , every elements in edge, that pointing to uV need to be changed to uG
-	        int a = 1 ;
+	        //after merge , every elements in edge, that pointing to uV need to be changed to uG
 	        graph.put(u,toMerge);
+	        graph.remove(v);
 
+	        // This algorithm need to be improved
 	        for (Map.Entry<Integer,Integer[]> entry : graph.entrySet()) {
 	        	  Integer key = entry.getKey();
 	        	  Integer[] value = entry.getValue();
@@ -177,58 +91,88 @@ public class KargerMinCut {
 	        	  for(int val : value){
 	        		  if(val==v){
 	        			  graph.get(key)[b]=u;
-	        //			  System.out.println("key = "+key + " b = "+b);
-	        	//		  System.out.println("val v "+v+" to "+ u);
-	        			  
-	        		  }if(key==val){
-	        			  graph.get(key)[b]=1000;
-	        			//  System.out.println("key = value");
-	        		//	  System.out.println("key = "+key+"   b = "+b);
 	        		  }
-	        		  
+	        			  
 	        		  b++;
 	        	  }
-	        	  a++;
 	        	}
 	        
-	        graph.remove(v);
-	        counter++;
-			//System.out.println(a);	
-		}
-		
-		//System.out.println("ended");
-		
-
-		
-		//System.out.println("");
+		} // end of while loop
 		
       	int mincutCount=0;
-		int trick = 0 ;
+		int firstHashMap = 0; // to count first hashmap only
 		for (Map.Entry<Integer,Integer[]> entry : graph.entrySet()) {
-      	  Integer key = entry.getKey();
-      	  Integer[] value = entry.getValue();
-      	  
-
-
-      	if(trick==0){
+      	Integer key = entry.getKey();
+      	Integer[] value = entry.getValue();
+      	if(firstHashMap==0){
       		for(int val : value){
-      			if(val!=1000&&val!=key){
+      			if(val!=1000&&val!=key){ // dont count self-loop 
       				mincutCount++;
       			}
       		}      		
       	}
-
-      	trick++;
+      	firstHashMap++;
       	}
 		
-		System.out.println(mincutCount+"!!!");
-      	
-      	
-      	
+		//System.out.println("Mincut = "+mincutCount);
       	return mincutCount;
 		
 		
 	}
 	
+	/*
+	 * Handeling input
+	 */
+	public HashMap<Integer,Integer[]> input() throws IOException{
+		String input = "src/algorithms/input/mincut.txt";
+		String inputTest = "src/algorithms/input/test.txt";
+		
+		FileInputStream fis = new FileInputStream(input);
+        DataInputStream in = new DataInputStream(fis);
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+		ArrayList<Integer[]> edges = new ArrayList<Integer[]>();
+		HashMap<Integer,Integer[]> graph = new HashMap<Integer,Integer[]>();  
+		
+		int count = 1;
+		int N = 200;
+		
+		while(count<=N){
+			Integer arr[];
+           arr = convertStringToInt(br.readLine().split("	"));
+           edges.add(extractEdges(arr));
+           graph.put(arr[0],extractEdges(arr));
+           
+           count++;
+		}
+		
+		return graph;
+		
+	}
+	
+	/*
+	 * Convert an array of String input to array of Integer
+	 */
+	public static Integer[] convertStringToInt(String array[]){
+		Integer[] intArr = new Integer[array.length];
+		for(int i = 0 ; i < array.length; i++) 
+			intArr[i] = Integer.parseInt(array[i]);
+		
+		return intArr;
+			
+	}
+	
+	/*
+	 * Disregard first element of an array
+	 */
+	public static Integer[] extractEdges(Integer arr[]){
+	
+		Integer edgesArr[] = new Integer[arr.length-1];
+		for(int i = 1 ; i < arr.length ; i ++ ){ // ignore first value of array since it represents vertex not edges
+			edgesArr[i-1] = arr[i];
+		}
+		
+		return edgesArr;
+	}
 
 }
